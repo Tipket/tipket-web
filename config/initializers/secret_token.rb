b7f5693710b1520c2ceac13c87c17c30224514aa
Luis Miguel Delgado <lmd411@gmail.com>
2013-10-25 23:13:00 -0430

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tipket::Application.config.secret_key_base = '0fb23ea76375cd8c49fc24c2b432daf69c8e98299ff10beeb0a177de11af8dde8d4f6b10617ffad78c2ce6327128ac20d575ba3e72d9cfc6d8964f430995d590'
